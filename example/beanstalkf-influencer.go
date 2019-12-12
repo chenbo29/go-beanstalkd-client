720ebde389e5e6c458d42e70096f59a2e161a432
chenbo <chenbotome@163.com>
2019-12-12 16:02:03 +0800

    influencer

--- example/beanstalkf-influencer.go
+++ example/beanstalkf-influencer.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"github.com/chenbo29/go-beanstalkd-client"
 	_ "github.com/chenbo29/gostorage"
+	_ "github.com/go-sql-driver/mysql"
 	"io"
 	"log"
 	"math/rand"
@@ -18,23 +20,48 @@ func main() {
 			logger := NewLog()
 			logger.Printf("jobID(%d) start to do jobBody(%s)", id, string(body))
 			var param struct {
-				FollowerNum      int
-				InfluencerUserId int
-				InfluencerPFId   int
+				FollowerNum       int
+				FollowerNumActual int
+				InfluencerUserId  int
+				InfluencerPFId    int
+				InfluencerPFDId   int
 			}
 			err := json.Unmarshal(body, &param)
 			if err != nil {
-				log.Fatal(err)
+				logger.Printf("error is %s", err)
+				return true
 			}
 			logger.Printf("follower_num is %d", param.FollowerNum)
 			var start = 12600
 			var end = 105000
-			for i := 0; i < param.FollowerNum; i++ {
-				logger.Printf("num is %d", i)
+			var userNum = 0
+			db, err := sql.Open("mysql", "linkcar:linkcar!)QP@tcp(47.100.225.249:3306)/linkcar_api")
+			if err != nil {
+				log.Fatal(err)
+			}
+			for i := 0; i < param.FollowerNumActual; i++ {
 				rand.Seed(time.Now().UnixNano())
 				var userId = rand.Intn(end-start) + start
-				logger.Printf("the distribution is %d", userId)
+				_, err = db.Exec("insert into user_follow (uid,follower_id) values (?, ?)", userId, param.InfluencerUserId)
+				if err != nil {
+					logger.Printf("error is %s", err)
+				}
+				userNum++
 			}
+			_, err = db.Exec("update influencer_post_follower set follower_num_actual = follower_num_actual+?, successful_num=successful_num+1 where id = ?", param.FollowerNumActual, param.InfluencerPFId)
+			if err != nil {
+				logger.Printf("influencer_post_follower error is %s", err)
+			}
+			_, err = db.Exec("update influencer_pf_detail set status = 'success', update_time = current_timestamp where id = ?", param.InfluencerPFDId)
+			if err != nil {
+				logger.Printf("influencer_pf_detail error is %s", err)
+			}
+			_, err = db.Exec("update user set follower_num = follower_num + ? where id = ?", param.FollowerNum, param.InfluencerUserId)
+			if err != nil {
+				logger.Printf("user error is %s", err)
+			}
+			logger.Printf("the user[%d] actual distribution user num is %d", param.InfluencerUserId, userNum)
+			db.Close()
 			return true
 		},
 	}
