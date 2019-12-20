f9a6f2e9ac44f847c3b4b101e034b76d57809716
chenbo <chenbotome@163.com>
2019-12-20 17:32:30 +0800

    ssh

--- example/influencer.sh
+++ example/influencer.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-go install beanstalkf-influencer.go
-go install beanstalkf-daemon.go
+go get -t github.com/chenbo29/go-beanstalkd-client
+go build beanstalkf-influencer.go
+go build beanstalkf-daemon.go
+cp beanstalkd-influencer /usr/bin/beanstalkd-influencer
+cp beanstalkd-daemon /usr/bin/beanstalkd-daemon
 echo "beanstalkf-influencer and beanstalkf-daemon install success"
