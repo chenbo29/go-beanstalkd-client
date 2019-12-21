7f5184a78387b7b66e712e31f73338a12e5db21e
chenbo <chenbotome@163.com>
2019-12-21 16:22:23 +0800

    ssh

--- example/influencer.sh
+++ example/influencer.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
+export GOBIN=/usr/bin
 go get -t github.com/chenbo29/go-beanstalkd-client
 go build beanstalkf-order.go
 go build beanstalkf-daemon.go
-cp beanstalkf-order /usr/bin/beanstalkd-order
-cp beanstalkf-daemon /usr/bin/beanstalkd-daemon
 echo "beanstalkf-order and beanstalkf-daemon install success"
