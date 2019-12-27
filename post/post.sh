89e62deb1be6e1d79972e1e675ee3e5e1fe30105
chenbo <chenbotome@163.com>
2019-12-27 17:57:04 +0800

    ssh

--- post/post.sh
+++ post/post.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 go build beanstalkf-post.go
 go build beanstalkf-post-daemon.go
-cp beanstalkf-post /usr/bin/beanstalkd-post
-cp beanstalkf-post-daemon /usr/bin/beanstalkd-post-daemon
+cp beanstalkf-post /usr/bin/beanstalkf-post
+cp beanstalkf-post-daemon /usr/bin/beanstalkf-post-daemon
 echo "beanstalkf-post and beanstalkf-post-daemon install success"
